cd311e003c6aeccc5d76f9b3aef6f3098fac2df2
无名组 <2419940233@qq.com>
2018-11-29 23:44:15 +0800

    some modifiy

--- homework/cnn_cifar/cifar10_loadModel_resnet20v2.py
+++ homework/cnn_cifar/cifar10_loadModel_resnet20v2.py
@@ -2,5 +2,5 @@
 from homework.cnn_cifar.cifar10_modelLoader import KerasModelLoader
 
 
-trainObj = KerasModelLoader('models/KerasResNetwork20v2/checkpoints.h5')
+trainObj = KerasModelLoader()
 trainObj.evaulateOnTest()
